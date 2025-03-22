25ef2022a1a9e695975dcfd3c9089bb043b2465e
Gleb Bashlychev <gbashlchychev@gmail.com>
2025-03-22 23:40:29 +0400

    test

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataType {
+    public static void main(String[] args) {
+        double maxRangeFloat = 12345.6789;
+        byte minRangeInt = 127;
+
+        System.out.println("double: " + maxRangeFloat);
+        System.out.println("byte: " + minRangeInt);
+    }
+}
